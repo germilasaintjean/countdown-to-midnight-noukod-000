f9dc36bd2e24bf012ca2cc564e8ed2c9c1500566
Saint-Jean Germila <germila@noukod.com>
2018-09-21 17:58:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,22 +1,8 @@
-def countdown
-     number = 0
-   loop do
-       puts "#{number} SECOND(S)!\n"
-       number += 1
-     if number == 12
-     break
-     return "HAPPY NEW YEAR!"
-   end
 
-def countdown_with_sleep(5)
-  real.to_i = 0
-  loop do
-    runtime = Benchmark.measure { countdown_with_sleep(5) }
-    real.to_i += 1
-     if (runtime.real.to_i >= 5)
-       break
-       true
-  end
-  end
-
+def countdown(seconds)
+  while seconds < 0
+    puts "#{number} SECOND(S)!"
+    seconds - = 1
+end
+retrurn "HAPPY NEW YEAR!"
 end
