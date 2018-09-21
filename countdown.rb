0d411542bb847afd2b4b045b3e946c7f50e3612a
Saint-Jean Germila <germila@noukod.com>
2018-09-21 17:43:35 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
-#write your code here
-
 def countdown
+     number = 0
+   loop do
+       puts "#{number} SECOND(S)!\n#{number}"
+       number += 1
+     break
+      if number == 12
+      puts "HAPPY NEW YEAR!"
+  end
 end
