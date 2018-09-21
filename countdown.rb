e8bbea81c04cc4ff4819b540f57ddbc5392df32e
Saint-Jean Germila <germila@noukod.com>
2018-09-21 18:05:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,14 @@
-def countdown
-     number = 0
+def countdown(number)
+     number = 10
    loop do
        puts "#{number} SECOND(S)!\n"
-       number += 1
-     if number == 12
+       sleep(1)
+       number -= 1
+     #if number == 12
      break
      return "HAPPY NEW YEAR!"
    end
-
+   
    def countdown_with_sleep (countdown)
      sleep(5)
    end
