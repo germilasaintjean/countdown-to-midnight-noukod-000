ade25892d8a83897a99d0aec0d33fa49617b80e4
Saint-Jean Germila <germila@noukod.com>
2018-09-21 17:55:30 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,22 @@
 def countdown
      number = 0
    loop do
-       puts "#{number} SECOND(S)!\n#{number}"
+       puts "#{number} SECOND(S)!\n"
        number += 1
+     if number == 12
      break
-      if number == 12
-      puts "HAPPY NEW YEAR!"
+     return "HAPPY NEW YEAR!"
+   end
+
+def countdown_with_sleep(5)
+  real.to_i = 0
+  loop do
+    runtime = Benchmark.measure { countdown_with_sleep(5) }
+    real.to_i += 1
+     if (runtime.real.to_i >= 5)
+       break
+       true
   end
+  end
+
 end
